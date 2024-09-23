99d06f6c28908d70cc24189cd9439e1e59a317e5
Mohammad Shokriyan <mohammad@tekschool.us>
2024-09-23 19:35:26 -0400

    adding new line of code

--- src/tek/example/Example1.java
+++ src/tek/example/Example1.java
@@ -6,5 +6,8 @@ public class Example1 {
   public static void main(String[] args) {
     int age = 20;
     System.out.println(age);
+
+    String name = "Mohammad";
+    System.out.println(name);
   }
 }
